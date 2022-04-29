5fad8b656ea1d73f4ec90b669fc278bbcf57eda2
Tashasun <natalia.limbakh@gmail.com>
2022-04-29 13:25:48 +0300

    feat: implement count-cats and dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let Array =[];
+  if (!members) {return false}
+  for (let i=0; i<members.length; i++){
+    if (typeof(members[i]) == "string"){
+      members[i] = members[i].trim();
+    Array.push(members[i][0].toUpperCase());
+ 
 }
+}
+
+if (Array.sort().join('') == "") {return false}
+return Array.sort().join('')
+
+}
+// console.log(createDreamTeam([
+//   'Amelia',
+//   'Grace',
+//   'Lily',
+//   'Ruby',
+// ]), 'AGLR')
 
 module.exports = {
   createDreamTeam
