83bd90ef4ca5101cc52dafc6931480e2fa6e202c
Tashasun <natalia.limbakh@gmail.com>
2022-04-29 13:06:57 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let Number = 0;
+  if (typeof sampleActivity !== "string") return false;
+  if (isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  }
+  {Number = Math.ceil(Math.log((15/sampleActivity))*5730/(693/1000))}
+  return Number;
 }
 
+// console.log(dateSample('one'), false);
+// console.log(dateSample('3'), 13305);
+// console.log(dateSample('1'), 22387);
 module.exports = {
   dateSample
 };
