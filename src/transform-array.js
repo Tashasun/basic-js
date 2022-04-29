43ba1c1254e16ff83939881ea1a2d9a4552273d1
Tashasun <natalia.limbakh@gmail.com>
2022-04-29 17:28:57 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) { throw new Error("'arr' parameter must be an instance of the Array!"); }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    try {
+      if (typeof arr[i] == "string") {
+        switch (arr[i]) {
+          case "--discard-next":
+            i = i + 1;
+            break;
+          case "--discard-prev":
+            newArr = newArr.splice(0, i - 1);
+            break;
+
+          case "--double-next":
+            if (arr[i + 1]) {
+              newArr.push(arr[i + 1])
+            }
+            break;
+          case "--double-prev":
+            if (arr[i - 1] || arr[i - 2]) {
+              if (arr[i - 2] == "--discard-next") {
+                break
+              }
+              else {
+                newArr.push(arr[i - 1]);
+                break;
+              }
+            }
+            else {
+              i++;
+            }
+          default: newArr.push(arr[i]);
+        }
+      }
+      else { newArr.push(arr[i]) }
+    }
+    catch (error) {
+      throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+  }
+  return newArr
 }
+
+// console.log(transform(['--discard-prev', 1, 2, 3]), "'--discard-prev'")
+// console.log(transform(['--double-prev', 1, 2, 3]), "'--double-prev'")
+// console.log(transform([1, 2, 3, '--double-next']), "'--double-next'")
+// console.log(transform([1, 2, 3, '--discard-next']), "'--discard-next'")
+
 
 module.exports = {
   transform
